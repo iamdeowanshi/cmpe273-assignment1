83da8994eb17170b89d895a4e6b62d305a3f469d
Aaditya Deowanshi <aadityadeowanshi@hotmail.com>
2017-03-16 14:15:59 -0700

    Changed Access Logic

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
-from flask import Flask
-from git import Repo
+from flask import Flask,render_template
+from github import Github
 
-import sys, shutil, os
-from os import listdir
-from os.path import isfile
+import sys,os
 
-app = Flask(__name__)
-
-local_path = "./config"
-
-def fetchRepo():
-    if (os.path.isdir(local_path)):
-        shutil.rmtree(local_path)
-    else:
-        pass
-
-    repo_url = str(sys.argv[1])
-    repository = Repo.clone_from(repo_url, local_path)
+template_dir = os.path.abspath('./html')
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route("/v1/<filename>")
 def showMessage(filename):
-    if isfile(local_path+"/"+filename):
-        file = open(local_path+"/"+filename) 
-        return file.read()
+    g = Github()
+    repo_url = str(sys.argv[1]).split("/")
+
+    repo = g.get_user(repo_url[3]).get_repo(repo_url[4])
+
+    file = repo.get_file_contents(filename)
+
+    if file is None:
+        page_not_found
+
+    if (filename.endswith(".yml")) or (filename.endswith(".json")):
+        return file.decoded_content
     else:
-        return "Page not found"
+        return render_template('403.html'), 403
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route("/")
 def hello():
-    return "Hello from Dockerized Flask App!!"
+    return "Hello from Dockerized Flask App!! "
 
 if __name__ == "__main__":
-    fetchRepo()
     app.run(debug=True,host='0.0.0.0')
